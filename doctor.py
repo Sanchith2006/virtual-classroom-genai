d1ecd1d1fab63030a2f96613f6abae01edfab0fe
Sanchith2006 <111049674+Sanchith2006@users.noreply.github.com>
2024-02-04 08:23:56 +0530

    Add files via upload

---
+++ doctor.py
@@ -0,0 +1,17 @@
+﻿import viz
+import vizfx
+import vizinfo
+import vizact
+import viztask
+
+viz.MainView.getHeadLight().disable()
+sky_light = viz.addDirectionalLight(euler=(0,20,0))
+sky_light.color(viz.WHITE)
+sky_light.ambient([0.8]*3)
+viz.setOption('viz.lightModel.ambient',[0]*3)
+TRIAL_COUNT = 5				
+TRIAL_DURATION = 20	
+env=vizfx.addChild('D:\\details\\sensei.osgb')
+
+viz.go()
+
